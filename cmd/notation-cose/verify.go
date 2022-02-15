e097c70a8fd5c7bfe34870b70bda3ba5ed08aa81
Shiwei Zhang <shizh@microsoft.com>
2022-02-15 14:07:22 +0800

    verify cose
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- cmd/notation-cose/verify.go
+++ cmd/notation-cose/verify.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/x509"
 	"encoding/json"
 	"errors"
+	"os"
 
+	"github.com/notaryproject/notation-go-lib"
+	"github.com/notaryproject/notation-go-lib/crypto/cryptoutil"
+	"github.com/shizhMSFT/notation-cose/pkg/cose"
 	"github.com/shizhMSFT/notation-cose/pkg/protocol"
 	"github.com/urfave/cli/v2"
 )
@@ -29,7 +34,34 @@ func runVerify(ctx *cli.Context) error {
 	}
 
 	// verify signature
+	verifier, err := getVerifier(req.KMSProfile)
+	if err != nil {
+		return err
+	}
+	desc, err := verifier.Verify(ctx.Context, req.Signature, req.VerifyOptions)
+	if err != nil {
+		return err
+	}
+	out, err := json.Marshal(desc)
+	if err != nil {
+		return err
+	}
 
 	// write response
-	return nil
+	_, err = os.Stdout.Write(out)
+	return err
+}
+
+func getVerifier(profile protocol.KMSProfileSuite) (notation.Verifier, error) {
+	bundledCerts, err := cryptoutil.ReadCertificateFile(profile.ID)
+	if err != nil {
+		return nil, err
+	}
+	roots := x509.NewCertPool()
+	for _, cert := range bundledCerts {
+		roots.AddCert(cert)
+	}
+	verifier := cose.NewVerifier()
+	verifier.VerifyOptions.Roots = roots
+	return verifier, nil
 }
